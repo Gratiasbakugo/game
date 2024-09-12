7df07fccc298b21d9b95877fb4bd14f4e12a7260
Rubens BOKO <119358678+Gratiasbakugo@users.noreply.github.com>
2024-09-12 19:55:11 +0100

    Update form.php

--- form.php
+++ form.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nom = htmlspecialchars($_POST['nom']);
     $prenom = htmlspecialchars($_POST['prenom']);
